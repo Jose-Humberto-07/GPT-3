ae11930449925d2e0d095bc462068f2073dce4aa
José Humberto <jhpnascimento96@gmail.com>
2022-02-02 23:13:49 -0300

    app style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import { Footer, Blog, Possibility, WhatGPT3, Header, Features } from './containers';
 import { Navbar, Brand, CTA } from './components';
+import './App.css';
 
 
 const App = () => {
